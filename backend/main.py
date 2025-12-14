1d7ef8a173628c3f92c9c37c8ce3772790ecabc7
Dylan Gee <dylangee3742@gmail.com>
2025-12-14 14:45:49 +0000

    Delete unused imports

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,4 @@
-from utils import load_file_as_image, ocr_images, preprocessing, save_preprocessed_images, run_audiveris
-
-# if __name__ == "__main__":
-#     test_file = "example.pdf"  # or "example.png", "example.jpg"
-
-#     # 1. Load file as images
-#     images = load_file_as_image({"data": {"file": "./Solfeggio in C minor, H. 220 - Complete score (Pierre Gouin).pdf"}})
-
-#     print(f"Loaded {len(images)} images from {"./Solfeggio in C minor, H. 220 - Complete score (Pierre Gouin).pdf"}")
-
-#     # 2. Run OCR on all images
-#     preprocessed_images = preprocessing(images)
-
-#     # 3. Output result
-#     print("---------- OCR Result ----------")
-#     print(extracted_text)
-#     print("--------------------------------")
-
-#     # 4. Optional: save preprocessed images to check results visually
-#     for i, img in enumerate(images):
-#         preprocessed = preprocessing(img)
-#         preprocessed.save(f"preprocessed_page_{i+1}.png")
-#         print(f"Saved preprocessed_page_{i+1}.png")
+from utils import load_file_as_image, save_preprocessed_images, run_audiveris
 
 if __name__ == "__main__":
 
